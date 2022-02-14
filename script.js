5fc8c285d4f53f25ed0d8fd326cadf8ea3e9e59c
Kaleb Bridges <kalebbridges@Kalebs-MBP.lan>
2022-02-13 23:56:21 -0600

    commit message

---
+++ script.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let button = document.createElement("button");
+  let btnText = document.createTextNode("Add Square");
+  button.appendChild(btnText);
+  document.body.appendChild(button);
+
+  let divButton = document.querySelector(".button");
+  let numCount = 0;
+
+  const colorArr = [
+    "black",
+    "pink",
+    "purple",
+    "gold",
+    "goldenrod",
+    "red",
+    "blue",
+    "yellow",
+  ];
+
+  button.addEventListener("click", function () {
+    let newDiv = document.createElement("div");
+    document.body.appendChild(newDiv);
+
+    newDiv.style.background = "Black";
+    newDiv.classList.add("Bbox");
+    numCount++;
+
+    newDiv.setAttribute("id", numCount);
+    let displaynum = document.createTextNode(numCount);
+    newDiv.appendChild(displaynum);
+    newDiv.addEventListener("mouseover", function () {
+      newDiv.style.color = "white";
+      newDiv.style.textAlign = "center";
+      newDiv.addEventListener("mouseleave", function () {
+        newDiv.style.color = "black";
+        newDiv.addEventListener("click", setRandomColor);
+        newDiv.addEventListener("dblclick", function () {
+          let childTwo = newDiv.nextElementSibling;
+
+          if (newDiv.textContent % 2 == 0 || newDiv.textContent % 2 == 1) {
+            childTwo.remove();
+          } else if (newDiv == null){
+            alert("No more boxes");
+          }
+        });
+      });
+    });
+  });
+  function setRandomColor() {
+    const randomNum = Math.floor(Math.random() * colorArr.length);
+    event.target.style.background = colorArr[randomNum];
+  }
+});
